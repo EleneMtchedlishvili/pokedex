07162d9f1689566e1ef26daa7b3b44862acea5ce
EleneMtchedlishvili <112971990+EleneMtchedlishvili@users.noreply.github.com>
2022-11-22 20:09:56 +0400

    pokedex

---
+++ script.js
@@ -0,0 +1,13 @@
+const API_URL = "https://pokeapi.co/api/v2/pokemon/";
+
+const header = document.getElementById("header");
+const container = document.getElementById("container");
+const pokemon = document.getElementById("pokemon");
+
+// for (let i = 1; i <= pokemon_count; i++) {
+//   await getPokemon(i);
+// }
+
+// const getPokemon = async (id) => {
+//   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+// };
